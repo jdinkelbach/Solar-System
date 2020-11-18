081b937e9b55959b16b33b720b8e85ab234788ee
Justin <jmdinkelbach@gmail.com>
2020-11-18 21:00:50 +0000

    Mercury/venus routes added

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", function(req, res){
     res.send("it works!"); 
 });
 
+app.get("/mercury", function(req, res){
+    res.send("Mercury webpage"); 
+});
+
+app.get("/venus", function(req, res){
+    res.send("Venus webpage"); 
+});
+
 // Starting Server (PORT, IP)
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running...");
